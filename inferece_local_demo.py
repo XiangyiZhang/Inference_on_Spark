8ba76149327545759264c976185b49bc646e331f
Xiangyi Zhang <62898839+XiangyiZhang@users.noreply.github.com>
2021-11-01 19:55:00 -0400

    Delete inferece_local_demo.py

--- inferece_local_demo.py
+++
@@ -1,9 +0,0 @@
-#from pyspark import SparkContext
-#sc = SparkContext("local", "collect app")
-import tensorflow as tf
-#v_1 = sc.parallelize(tf.constant([1,2,3,4]))
-v_1 = tf.constant([1,2,3,4])
-v_add = tf.math.reduce_sum(v_1)
-with tf.Session() as sess:
-    print(sess.run(v_add))
-
